692132cc7195f7f595ea903a4daab5205a946043
Johnson <yeslee1126@gmail.com>
2020-07-03 14:54:27 +0800

    add analytics

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -11,31 +11,8 @@ import 'prismjs/themes/prism.css'
 import './src/styles/global.scss'
 
 export function onRouteUpdate () {
-  // const bp = document.createElement('script')
-  // bp.id = 'baidu-push'
-  // const curProtocol = window.location.protocol.split(':')[0]
-  // if (curProtocol === 'https') {
-  //   bp.src = 'https://zz.bdstatic.com/linksubmit/push.js?t=' + new Date().getTime()
-  // } else {
-  //   bp.src = 'http://push.zhanzhang.baidu.com/push.js?t=' + new Date().getTime()
-  // }
-  // const old = document.getElementById('baidu-push')
-  // if (old) {
-  //   old.src = bp.src
-  // } else {
-  //   window.document.body.appendChild(bp)
-  // }
-
-  // const adsense = document.createElement('script')
-  // adsense.id = 'adsense'
-  // adsense.async = true
-  // adsense.src = '//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?t=' + new Date().getTime()
-  // adsense.setAttribute('data-ad-client', 'ca-pub-1108685461241651')
-
-  // const oldAd = document.getElementById('adsense')
-  // if (oldAd) {
-  //   oldAd.src = adsense.src
-  // } else {
-  //   window.document.head.appendChild(adsense)
-  // }
+  // 谷歌统计
+  if (window.gtag && typeof window.gtag === 'function') {
+    window.gtag('config', 'UA-171547937-1')
+  }
 }
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -13,7 +13,40 @@ exports.onRenderBody = ({
   setPostBodyComponents,
   setHeadComponents
 }) => {
-  // setHeadComponents([])
+  setHeadComponents([
+    <script key='GOOGLE_GTAG' async src="https://www.googletagmanager.com/gtag/js?id=UA-171547937-1"></script>,
+    <script key='GOOGLE_ANALYTICS' dangerouslySetInnerHTML={{
+      __html: `
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+      
+        gtag('config', 'UA-171547937-1');
+      `
+    }}/>,
+    <script key='BAIDU_STATISTIC_SPA_TRACER' dangerouslySetInnerHTML={{
+      __html: `
+        var _hmt = _hmt || [];
+        _hmt.push(['_requirePlugin', 'UrlChangeTracker', {
+          shouldTrackUrlChange: function (newPath, oldPath) {
+          return newPath && oldPath;
+          }}
+        ]);
+      `
+    }}/>,
+    <script key='BAIDU_STATISTIC' dangerouslySetInnerHTML={{
+      __html: `
+        var _hmt = _hmt || [];
+        (function() {
+          const hm = document.createElement("script");
+          hm.src = "https://hm.baidu.com/hm.js?3872d781f970a63603d95f9f88a6497e";
+          var s = document.getElementsByTagName("script")[0]; 
+          s.parentNode.insertBefore(hm, s);
+        })();
+      `
+    }} />
+  ])
   setPostBodyComponents([
     <script async src="//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js" key='bsz'></script>
   ])
