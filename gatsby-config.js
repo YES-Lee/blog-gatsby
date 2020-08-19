cc8c7d209b5dc0c8c7efbf6325f635b5960a7797
Johnson <yeslee1126@gmail.com>
2020-08-19 13:37:10 +0800

    update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -248,7 +248,7 @@ module.exports = {
               }
             `,
             output: '/rss.xml',
-            title: "Johnson's Feed",
+            title: 'Johnson的博客',
             // optional configuration to insert feed reference in pages:
             // if `string` is used, it will be used to create RegExp and then test if pathname of
             // current page satisfied this regular expression;
